1039c4eaf33958750eeaa4de5cf9567469aed30e
danielspring-crypto <borncode3@gmail.com>
2020-09-07 05:05:47 -0400

    first commit

---
+++ courses/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.forms.models import inlineformset_factory
+from .models import Course, Module, Comment
+
+
+ModuleFormSet = inlineformset_factory(Course,
+                                      Module,
+                                      fields=['title', 'description'],
+                                      extra=2,
+                                      can_delete=True)
+
+
+
+class CommentForm(forms.ModelForm):
+	class Meta:
+		model = Comment 
+		fields = ('body',)
